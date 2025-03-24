fe0164ff43a7c97a786639dea622e5777796f0cb
prajwalshetti <157411285+prajwalshetti@users.noreply.github.com>
2025-03-24 15:39:02 +0530

    added insta link

--- src/components/global/contactsDeck.tsx
+++ src/components/global/contactsDeck.tsx
@@ -1,44 +1,50 @@
 import React from "react";
 import { FloatingDock } from "@/components/ui/floating-dock";
 import {
-    IconBrandGithub,
-    IconBrandX,
-    IconBrandLinkedin,
-    IconMail,
-    IconBrandInstagram
+  IconBrandGithub,
+  IconBrandLinkedin,
+  IconMail,
+  IconBrandInstagram
 } from "@tabler/icons-react";
-import url from "@/data/projectData.json"
-export function FloatingDockDemo() {
-    const links = [
-        {
-            title: "GitHub",
-            icon: (
-                <IconBrandGithub className="h-full w-full text-neutral-500 dark:text-neutral-300" />
-            ),
-            href: `${url.personal_info.my_github}`
-        },
-        {
-            title: "Linkedin",
-            icon: (
-                <IconBrandLinkedin className="h-full w-full text-neutral-500 dark:text-neutral-300" />
-            ),
-            href: `${url.personal_info.my_linkedin}`
-        },
-        {
-            title:"Gmail",
-            icon:(
-                <IconMail className="h-full w-full text-neutral-500 dark:text-neutral-300" />
-            ),
-            href:`mailto:${url.personal_info.my_email}`,
-        }
+import url from "@/data/projectData.json";
 
-    ];
-    return (
-        <div className="flex items-center justify-center max-h-14 w-full">
-            <FloatingDock
-                mobileClassName="translate-y-20"
-                items={links}
-            />
-        </div>
-    );
+export function FloatingDockDemo() {
+  const { my_github, my_linkedin, my_email, my_instagram } = url.personal_info;
+
+  const links = [
+    {
+      title: "GitHub",
+      icon: (
+        <IconBrandGithub className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: my_github,
+    },
+    {
+      title: "LinkedIn",
+      icon: (
+        <IconBrandLinkedin className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: my_linkedin,
+    },
+    {
+      title: "Gmail",
+      icon: (
+        <IconMail className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: `mailto:${my_email}`,
+    },
+    {
+      title: "Instagram",
+      icon: (
+        <IconBrandInstagram className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: my_instagram,
+    }
+  ];
+
+  return (
+    <div className="flex items-center justify-center max-h-14 w-full">
+      <FloatingDock mobileClassName="translate-y-20" items={links} />
+    </div>
+  );
 }
